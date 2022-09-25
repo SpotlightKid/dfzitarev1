9eafaae34f4a04a3dde55e417f08cbb50fb9e34e
Christopher Arndt <chris@chrisarndt.de>
2022-09-25 12:51:48 +0200

    Improve release build scripts
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- scripts/build-win64.sh
+++ scripts/build-win64.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+# Build win32 binaries of VST2/3 and LV2 plugin
+#
+
+PROJECT_VERSION="$(git describe --abbrev=0 2>/dev/null)"
+
+if [[ -z "$PROJECT_VERSION" ]]; then
+    echo "WARNING: No git tags found. Can't create binary distribution archive"
+    echo "Hint: Use 'git tag -a <version>' to create a project version."
+    echo
+fi
 
 set -e
 
@@ -24,5 +35,16 @@ make -C dpf clean > /dev/null
 rm -rf bin-w64
 
 # Build now
-make HAVE_CAIRO=false HAVE_JACK=false
+make HAVE_CAIRO=false HAVE_JACK=false BUILD_VST2=true
 mv bin bin-w64
+
+# Make Zip archive of binaries
+if [[ -n "$PROJECT_VERSION" ]]; then
+    CHECKOUT="$(pwd)"
+    REPO_URL="$(git remote get-url origin)"
+    PROJECT_NAME="${REPO_URL##*/}"
+    SRCDIR="$PROJECT_NAME-${PROJECT_VERSION#v}"
+    ZIP_NAME="$SRCDIR-win64.zip"
+    mkdir -p dist
+    rm -f dist/$ZIP_NAME && zip -r dist/$ZIP_NAME bin-w64
+fi
--- scripts/bundle-source.sh
+++ scripts/bundle-source.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+#
+# Buill source distribution archive with all git sub-modules included
+#
 
 PROJECT_VERSION="$(git describe --abbrev=0 2>/dev/null)"
 
 if [[ -z "$PROJECT_VERSION" ]]; then
-    echo "No git tags found. Use 'git tag -a <version>' to create a project version."
+    echo "ERROR: No git tags found. Can't create source distribution archve."
+    echo "Hint: Use 'git tag -a <version>' to create a project version."
     exit 1
 fi
 
@@ -15,9 +19,10 @@ PROJECT_NAME="${REPO_URL##*/}"
 PROJECT_NAME="${PROJECT_NAME%.git}"
 SRCDIR="$PROJECT_NAME-${PROJECT_VERSION#v}"
 TARBALL_NAME="$SRCDIR-source.tar.gz"
+BUILDDIR="build/${PROJECT_VERSION#v}"
 
-mkdir -p build dist
-cd build
+mkdir -p "$BUILDDIR" dist
+cd "$BUILDDIR"
 rm -rf "$SRCDIR"
 git clone --recursive --branch "$PROJECT_VERSION" --single-branch "$CHECKOUT" "$SRCDIR"
 cd "$SRCDIR"
@@ -26,9 +31,9 @@ for fn in .git .gitmodules .gitignore; do
 done
 rm -rf .git
 cd ..
-tar -zcvf "../dist/$TARBALL_NAME" "$SRCDIR"
-rm -rf "$SRCDIR"
+tar -zcvf "$CHECKOUT/dist/$TARBALL_NAME" "$SRCDIR"
 
-gpg --armor --detach-sign --yes "../dist/$TARBALL_NAME"
-cd ..
+gpg --armor --detach-sign --yes "$CHECKOUT/dist/$TARBALL_NAME"
+cd "$CHECKOUT"
+rm -rf "$BUILDDIR"
 ls -l dist
